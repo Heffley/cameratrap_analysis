b4f24bc22183cdfb7436d9f122887bd60d5d748d
Jeffrey Chen <jheff.chen13@gmail.com>
2022-07-15 11:37:24 -0400

    Update single-species occupancy.R
    Updated for single species occupancy for deer. Added code for plots, notes, and code to set up the occupancy models

--- single-species occupancy.R
+++ single-species occupancy.R
@@ -46,12 +46,30 @@ cov <- as.data.frame(scale(cov))
 ##call detection covariate matrix here if using
 ##det_list <- list(season = det_covs)
 
+# setting up for occupancy for deer
+y <- detection_matrix$deer[ , 2:54]
+
+siteCovs_500 <- as.data.frame(b500)
+siteCovs_500 <- siteCovs_500[,2:28]
+
+siteCovs_1000 <- as.data.frame(b1000)
+siteCovs_1000 <- siteCovs_500[,2:28]
+
+siteCovs_2000 <- as.data.frame(b2000)
+siteCovs_2000 <- siteCovs_500[,2:28]
+
+umf_deer_500 <- unmarkedFrameOccu(y = y, siteCovs = siteCovs_500)
+umf_deer_1000 <- unmarkedFrameOccu(y = y, siteCovs = siteCovs_1000)
+umf_deer_2000 <- unmarkedFrameOccu(y = y, siteCovs = siteCovs_2000)
+
 ##SINGLE SPECES
 y_list_c <- list(coyote = as.matrix(detection_matrix$coyote %>% select(-1)))
 coyote <- unmarkedFrameOccu(y = (detection_matrix$coyote%>%select(-1)),
                             siteCovs = cov)
 #obsCovs = det_list
-mdata <- coyote
+#mdata <- coyote
+## in order to change deer model with buffer change swap mdata variable with umf_deer_500, umf_deer_1000, or umf_deer_2000
+mdata <- umf_deer_2000
 
 
 ##single covariate comparison
@@ -62,7 +80,7 @@ fit_null <- occu(formula = ~ 1
 
 
 fit_null <- occu(formula = ~1
-                      ~1
+                      ~1,
                  data = mdata)
 
 fit_LFT <- occu(formula = ~1
@@ -130,7 +148,7 @@ fit_WVO_PA <- occu(formula = ~1
 
 fit_WVF_PA <- occu(formula = ~1
                         ~WVF_PA,
-                        
+    
                         data = mdata)
 
 fit_MV_PA <- occu(formula = ~1
@@ -179,6 +197,51 @@ coy_SOM <- occu(formula = ~ 1
                 data = mdata)
 coy_best_fit_cov_list <- dredge(coy_SOM, 
                                 rank = "AIC")
+# plots for deer single occupancy
+dem_med_data <- data.frame(DEM_median=seq(60, 180, by=1))
+occ.prob.dem_med <- predict(fit_DEM_median, type="state", newdata=dem_med_data, appendData=TRUE)
+
+dem_mdn_plot <- plot(Predicted ~ DEM_median, occ.prob.dem_med, type="l", ylim=c(0,1),
+                          xlab="DEM Median",
+                          ylab="Expected occupancy probability")
+lines(lower ~ DEM_median, occ.prob.dem_med, type="l", col=gray(0.5))
+lines(upper ~ DEM_median, occ.prob.dem_med, type="l", col=gray(0.5))
+
+ndvi_mean_data <- data.frame(NDVI_mean=seq(0.15, 0.5, by=0.01))
+occ.prob.ndvi_mean <- predict(fit_NDVI_mean, type="state", newdata=ndvi_mean_data, appendData=TRUE)
+
+ndvi_mean_plot <- plot(Predicted ~ NDVI_mean, occ.prob.ndvi_mean, type="l", ylim=c(0,1),
+                            xlab="NDVI Mean",
+                            ylab="Expected occupancy probability")
+lines(lower ~ NDVI_mean, occ.prob.ndvi_mean, type="l", col=gray(0.5))
+lines(upper ~ NDVI_mean, occ.prob.ndvi_mean, type="l", col=gray(0.5))
+
+wvf_dist_data <- data.frame(WVF_dist=seq(0, 2000, by=1))
+occ.prob.wvf_dist <- predict(fit_WVF, type="state", newdata=wvf_dist_data, appendData=TRUE)
+
+wvf_dist_plot <- plot(Predicted ~ WVF_dist, occ.prob.wvf_dist, type="l", ylim=c(0,1),
+                           xlab="WVF Dist",
+                           ylab="Expected occupancy probability")
+lines(lower ~ WVF_dist, occ.prob.wvf_dist, type="l", col=gray(0.5))
+lines(upper ~ WVF_dist, occ.prob.wvf_dist, type="l", col=gray(0.5))
+
+wvf_pa_data <- data.frame(WVF_PA=seq(0, 0.14, by=0.001))
+occ.prob.wvf_pa <- predict(fit_WVF_PA, type="state", newdata=wvf_pa_data, appendData=TRUE)
+
+wvf_pa_plot <- plot(Predicted ~ WVF_PA, occ.prob.wvf_pa, type="l", ylim=c(0,1),
+                      xlab="WVF PA",
+                      ylab="Expected occupancy probability")
+lines(lower ~ WVF_PA, occ.prob.wvf_pa, type="l", col=gray(0.5))
+lines(upper ~ WVF_PA, occ.prob.wvf_pa, type="l", col=gray(0.5))
 
 ###notes for interpretation
-##at 1000 buffer, built was the best model , however not under 2 AIC score from null, just 0.62
+## at 1000 buffer, built was the best model , however not under 2 AIC score from null, just 0.62
+
+## for the 500 and 1000 buffer with deer, the best models were WVF Dist, DEM Median, DEM Mean, NDVI Mean, and NDVI Median
+## in that order with AIC values all within at most 1.6 difference
+
+## for 2000 buffer with deer, the best models were NDVI Mean, NDVI Median and WVF PA
+## in that order with AIC values all within at most 0.58 difference
+
+## for the plots of the deer occupancy, the x-axis of the plots will be needed to changed according to the range
+## of the covariates
